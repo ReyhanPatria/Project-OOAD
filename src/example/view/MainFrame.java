515c20a9e7f052a139ce6d30b7693e9a31a49633
ReyhanPatria <reyhan2patria@gmail.com>
2020-08-13 01:46:56 +0700

    Set minimum, maximum, and preferred sizes

--- src/example/view/MainFrame.java
+++ src/example/view/MainFrame.java
@@ -1,6 +1,7 @@
 package example.view;
 
 import java.awt.Dimension;
+import java.awt.GraphicsEnvironment;
 
 import javax.swing.JFrame;
 
@@ -9,13 +10,22 @@ public class MainFrame extends JFrame {
 	
 	private static MainFrame instance;
 	
-	private static Integer WIDTH = 800;
-	private static Integer HEIGHT = 450;
+	public final static Dimension SCREEN_SIZE = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().getSize();
+	
+	private final static Integer MINIMUM_SIZE_WIDTH = (int) (SCREEN_SIZE.getWidth() * 0.5);
+	private final static Integer MINIMUM_SIZE_HEIGHT = (int) (SCREEN_SIZE.getHeight() * 0.5);
+	public final static Dimension MINIMUM_SIZE = new Dimension(MINIMUM_SIZE_WIDTH, MINIMUM_SIZE_HEIGHT);
+	
+	private final static Integer PREFERRED_SIZE_WIDTH = (int) (SCREEN_SIZE.getWidth() * 0.8);
+	private final static Integer PREFERRED_SIZE_HEIGHT = (int) (SCREEN_SIZE.getHeight() * 0.8);
+	public final static Dimension PREFERRED_SIZE = new Dimension(PREFERRED_SIZE_WIDTH, PREFERRED_SIZE_HEIGHT);
+	
 	
 	public MainFrame() {
-		this.setSize(WIDTH, HEIGHT);
-		this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-		this.setMinimumSize(new Dimension(WIDTH, HEIGHT));
+		this.setSize(PREFERRED_SIZE);
+		this.setMinimumSize(MINIMUM_SIZE);
+		this.setPreferredSize(PREFERRED_SIZE);
+		this.setMaximumSize(SCREEN_SIZE);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		
