169ea79800d9a915ad9e2ab7adde3da382d466f1
ReyhanPatria <reyhan2patria@gmail.com>
2020-08-13 16:53:19 +0700

    Added Login panel title

--- src/example/view/LoginPanel.java
+++ src/example/view/LoginPanel.java
@@ -33,10 +33,20 @@ public class LoginPanel extends JPanel {
 		 */
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{0, 118, 0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0, 30, 0, 0, 30, 0, 30, 0, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 30, 0, 0, 30, 0, 0, 30, 0, 30, 0, 0, 0, 0};
 		gridBagLayout.columnWeights = new double[]{1.0, 0.0, 1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
+		
+		/*
+		 * Panel title label
+		 */
+		JLabel panelTitleLabel = new JLabel("Login");
+		GridBagConstraints gbc_panelTitleLabel = new GridBagConstraints();
+		gbc_panelTitleLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_panelTitleLabel.gridx = 1;
+		gbc_panelTitleLabel.gridy = 1;
+		add(panelTitleLabel, gbc_panelTitleLabel);
 		
 		/*
 		 * Adding Username TextField and Label
@@ -46,7 +56,7 @@ public class LoginPanel extends JPanel {
 		gbc_usernameLabel.anchor = GridBagConstraints.WEST;
 		gbc_usernameLabel.insets = new Insets(0, 0, 5, 5);
 		gbc_usernameLabel.gridx = 1;
-		gbc_usernameLabel.gridy = 1;
+		gbc_usernameLabel.gridy = 3;
 		add(usernameLabel, gbc_usernameLabel);
 		
 		usernameTextField = new JTextField();
@@ -55,7 +65,7 @@ public class LoginPanel extends JPanel {
 		gbc_usernameTextField.fill = GridBagConstraints.HORIZONTAL;
 		gbc_usernameTextField.insets = new Insets(0, 0, 5, 5);
 		gbc_usernameTextField.gridx = 1;
-		gbc_usernameTextField.gridy = 2;
+		gbc_usernameTextField.gridy = 4;
 		add(usernameTextField, gbc_usernameTextField);
 		usernameTextField.setColumns(10);
 		
@@ -67,7 +77,7 @@ public class LoginPanel extends JPanel {
 		gbc_passwordLabel.anchor = GridBagConstraints.WEST;
 		gbc_passwordLabel.insets = new Insets(0, 0, 5, 5);
 		gbc_passwordLabel.gridx = 1;
-		gbc_passwordLabel.gridy = 4;
+		gbc_passwordLabel.gridy = 6;
 		add(passwordLabel, gbc_passwordLabel);
 		
 		passwordTextField = new JPasswordField();
@@ -76,7 +86,7 @@ public class LoginPanel extends JPanel {
 		gbc_passwordTextField.fill = GridBagConstraints.HORIZONTAL;
 		gbc_passwordTextField.insets = new Insets(0, 0, 5, 5);
 		gbc_passwordTextField.gridx = 1;
-		gbc_passwordTextField.gridy = 5;
+		gbc_passwordTextField.gridy = 7;
 		add(passwordTextField, gbc_passwordTextField);
 		passwordTextField.setColumns(10);
 		
@@ -101,7 +111,7 @@ public class LoginPanel extends JPanel {
 		GridBagConstraints gbc_loginButton = new GridBagConstraints();
 		gbc_loginButton.insets = new Insets(0, 0, 5, 5);
 		gbc_loginButton.gridx = 1;
-		gbc_loginButton.gridy = 7;
+		gbc_loginButton.gridy = 9;
 		add(loginButton, gbc_loginButton);
 		
 		/*
@@ -111,7 +121,7 @@ public class LoginPanel extends JPanel {
 		GridBagConstraints gbc_signupLabel = new GridBagConstraints();
 		gbc_signupLabel.insets = new Insets(0, 0, 5, 5);
 		gbc_signupLabel.gridx = 1;
-		gbc_signupLabel.gridy = 9;
+		gbc_signupLabel.gridy = 11;
 		add(signupLabel, gbc_signupLabel);
 		
 		JButton signupButton = new JButton("Sign up");
@@ -124,7 +134,7 @@ public class LoginPanel extends JPanel {
 		GridBagConstraints gbc_signupButton = new GridBagConstraints();
 		gbc_signupButton.insets = new Insets(0, 0, 5, 5);
 		gbc_signupButton.gridx = 1;
-		gbc_signupButton.gridy = 10;
+		gbc_signupButton.gridy = 12;
 		add(signupButton, gbc_signupButton);
 	}
 }
