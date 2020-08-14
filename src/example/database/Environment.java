4aedf3d3222ca011f7315cfb5fbd2544219ec307
ReyhanPatria <reyhan2patria@gmail.com>
2020-08-14 08:34:24 +0700

    Inital commit

---
+++ src/example/database/Environment.java
@@ -0,0 +1,10 @@
+package example.database;
+
+public class Environment {
+
+	public static String db_product = "mysql";
+	public static String db_name = "taskmanager_db";
+	public static String auth_username = "root";
+	public static String auth_password = "";
+
+}
