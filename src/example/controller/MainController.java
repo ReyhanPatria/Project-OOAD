f557c5bcea1b24140929a9c705b5c3f3027df0bb
ReyhanPatria <reyhan2patria@gmail.com>
2020-08-18 01:27:26 +0700

    Changed to not use Singleton pattern

--- src/example/controller/MainController.java
+++ src/example/controller/MainController.java
@@ -5,29 +5,10 @@ import javax.swing.JPanel;
 import example.view.MainFrame;
 
 public class MainController {
-	private static MainController instance;
-	
-	private MainFrame mainFrame;
-	
-	public MainController() {
-		mainFrame = MainFrame.getInstance();
-	}
-	
-	public static MainController getInstance() {
-		if(instance == null) {
-			instance = new MainController();
-		}
-		return instance;
-	}
-	
-	public void changePanel(JPanel newPanel) {
-		mainFrame.invalidate();
-		mainFrame.setContentPane(newPanel);
-		mainFrame.revalidate();
-		mainFrame.repaint();
-	}
-	
-	public MainFrame getMainFrame() {
-		return mainFrame;
+	public static void changePanel(JPanel newPanel) {
+		MainFrame.getInstance().invalidate();
+		MainFrame.getInstance().setContentPane(newPanel);
+		MainFrame.getInstance().revalidate();
+		MainFrame.getInstance().repaint();
 	}
 }
