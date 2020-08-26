d62e18941649bc650fd0ced21e28381c09d28956
ReyhanPatria <reyhan2patria@gmail.com>
2020-08-27 03:34:51 +0700

    Resolved merge conflicts

--- src/example/controller/UserController.java
+++ src/example/controller/UserController.java
@@ -1,6 +1,5 @@
 package example.controller;
 
-import java.io.InvalidObjectException;
 import java.rmi.NoSuchObjectException;
 import java.sql.Date;
 import java.util.List;
@@ -21,6 +20,7 @@ public class UserController {
 	// STATIC ATTRIBUTES ----------------------------------------------------
 	private static UserController instance;
 	
+	public static String[] allRoleList = {"admin", "supervisor", "worker"};
 	public static String[] selectableRoleList = {"supervisor", "worker"};
 	
 	
@@ -34,8 +34,26 @@ public class UserController {
 	}
 	
 	// Save updates on currently logged in User’s attributes to database
-	public User updateProfile(String username, Date DOB, String address,
-			String telp) throws InvalidObjectException {
+	public User updateProfile(String username, Date DOB, String address, String telp) 
+			throws IllegalArgumentException {
+		// Validate parameters
+		if(validateUsername(username) == false) {
+			throw new IllegalArgumentException("Username has been taken");
+		}
+		else if(validateUsernameLength(username) == false) {
+			throw new IllegalArgumentException("Username length has to be 5-15 characters");
+		}
+		else if(validateDOB(DOB) == false) {
+			throw new IllegalArgumentException("Date of Birth must be in the past");
+		}
+		else if(validateAddressLength(address) == false) {
+			throw new IllegalArgumentException("Address length must be 10-100 characters");
+		}
+		else if(validateTelp(telp) == false) {
+			throw new IllegalArgumentException("Telp must be all digits");
+		}
+		
+		
 		User currentUser = getUser(currentUserId);
 		
 		currentUser.setUsername	(username	);
@@ -49,14 +67,14 @@ public class UserController {
 	
 	// Changes currently logged in User password to a new password
 	public User changePassword(String oldPassword, String newPassword) 
-			throws InvalidObjectException {
+			throws IllegalArgumentException {
 		User currentUser = getUser(currentUserId);
 		
 		if(oldPassword.equals(currentUser.getPassword()) == false) {
 			throw new IllegalArgumentException("Incorrect old password");
 		}
 		else if(newPassword.length() <= 0) {
-			throw new IllegalAccessError("New password cannot be empty");
+			throw new IllegalArgumentException("New password cannot be empty");
 		}
 		
 		currentUser.setPassword(newPassword);
@@ -89,7 +107,7 @@ public class UserController {
 	
 	// Registers new User
 	public static User registerUser(String username, String password, String confirmPassword, String role,
-			String address, Date DOB, String telp) throws IllegalArgumentException, InvalidObjectException {
+			String address, Date DOB, String telp) throws IllegalArgumentException {
 		if(password.equals(confirmPassword) == false) {
 			throw new IllegalArgumentException("Password does not match!");
 		}
@@ -122,7 +140,7 @@ public class UserController {
 	
 	// Get a list of all user based on a role
 	public static List<User> getUserByRole(String role) throws IllegalArgumentException {
-		if(User.validateRole(role)) {
+		if(validateRole(role)) {
 			return User.getUserByRole(role);
 		}
 		
@@ -143,14 +161,27 @@ public class UserController {
 	// Creating a new User object and stores it in the Database.
 	public static User createUser(String username, String password, String role, 
 			Date DOB, String address, String telp) throws IllegalArgumentException {
+		if(validateUsername(username) == false) {
+			throw new IllegalArgumentException("Username has been taken");
+		}
+		else if(validateUsernameLength(username) == false) {
+			throw new IllegalArgumentException("Username length has to be 5-15 characters");
+		}
+		else if(validateRole(role) == false) {
+			throw new IllegalArgumentException("Invalid role");
+		}
+		else if(validateDOB(DOB) == false) {
+			throw new IllegalArgumentException("Date of Birth must be in the past");
+		}
+		else if(validateAddressLength(address) == false) {
+			throw new IllegalArgumentException("Address length must be 10-100 characters");
+		}
+		else if(validateTelp(telp) == false) {
+			throw new IllegalArgumentException("Telp must be all digits");
+		}
+		
 		User newUser = new User(UUID.randomUUID(), username, password, role, address, DOB, telp);
-		try {
-			newUser.save();
-		}
-		catch(InvalidObjectException e) {
-			throw new IllegalArgumentException(e.getMessage());
-//			e.printStackTrace();
-		}
+		newUser.save();
 		
 		return newUser;
 	}
@@ -162,13 +193,88 @@ public class UserController {
 	}
 	
 	// Resets of a user based on their ID
-	public static User resetPassword(UUID userID) 
-			throws InvalidObjectException {
+	public static User resetPassword(UUID userID) {
 		User u = getUser(userID);
 		
 		u.setPassword(u.getDOB().toString());
 		u.update();
 		
 		return u;
+	}
+	
+	
+	
+	
+	
+	// VALIDATORS ----------------------------------------------------
+	// Checks if id has been taken. TRUE if not taken, FALSE if taken
+	public static Boolean validateID(UUID id) {
+		List<User> userList = getAllUser();
+		for(User u: userList) {
+			if(id.toString().equals(u.getId().toString())) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	// Checks if username has been taken. TRUE if not taken, FALSE if taken
+	public static Boolean validateUsername(String username) {
+		List<User> userList = getAllUser();
+		for(User u: userList) {
+			if(username.equals(u.getUsername())) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	// Checks if username's length is correct. 5-15 characters
+	public static Boolean validateUsernameLength(String username) {
+		username = username.replace(" ", "");
+		if(username.length() < 5 || username.length() > 15) {
+			return false;
+		}
+		return true;
+	}
+	
+	// Checks if role is a valid role
+	public static Boolean validateRole(String role) {
+		for(String r: allRoleList) {
+			if(role.equalsIgnoreCase(r)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	// Checks if address length is valid. 10-100 characters
+	public static Boolean validateAddressLength(String address) {
+		address = address.replace(" ", "");
+		if(address.length() < 10 || address.length() > 100) {
+			return false;
+		}
+		return true;
+	}
+	
+	// Checks if DOB date is in the past
+	public static Boolean validateDOB(Date DOB) {
+		if(DOB.compareTo(new java.util.Date()) < 0) {
+			return true;
+		}
+		return false;
+	}
+	
+	// Checks if telp is all numbers
+	public static Boolean validateTelp(String telp) {
+		if(telp.length() <= 0) {
+			return false;
+		}
+		for(Integer i = 0; i < telp.length(); i++) {
+			if(Character.isDigit(telp.charAt(i)) == false) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
