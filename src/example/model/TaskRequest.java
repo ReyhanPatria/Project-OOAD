1b52e62d3eb6aa750e93b58ebe44262d0e48b6ee
ReyhanPatria <reyhan2patria@gmail.com>
2020-08-28 18:00:31 +0700

    Modified exception handling

--- src/example/model/TaskRequest.java
+++ src/example/model/TaskRequest.java
@@ -33,61 +33,44 @@ public class TaskRequest {
 	}
 	
 	// Inserts a new TaskRequest into database
-	public TaskRequest save() {
-		try {
-			PreparedStatement saveStatement = Connection.getConnection().prepareStatement(
-					"INSERT INTO `task_requests`(`id`, `supervisor_id`, `worker_id`, `title`, `note`) " + 
-					"VALUES (?, ?, ?, ?, ?)");
-			
-			saveStatement.setString(1, this.id.toString());
-			saveStatement.setString(2, this.supervisorID.toString());
-			saveStatement.setString(3, this.workerID.toString());
-			saveStatement.setString(4, this.title);
-			saveStatement.setString(5, this.note);
-			
-			saveStatement.executeUpdate();
-		}
-		catch(SQLException e) {
-			e.printStackTrace();
-		}
+	public TaskRequest save() throws SQLException {
+		PreparedStatement saveStatement = Connection.getConnection().prepareStatement(
+				"INSERT INTO `task_requests`(`id`, `supervisor_id`, `worker_id`, `title`, `note`) " + 
+				"VALUES (?, ?, ?, ?, ?)");
+		
+		saveStatement.setString(1, this.id.toString());
+		saveStatement.setString(2, this.supervisorID.toString());
+		saveStatement.setString(3, this.workerID.toString());
+		saveStatement.setString(4, this.title);
+		saveStatement.setString(5, this.note);
+		
+		saveStatement.executeUpdate();
 		
 		return this;
 	}
 	
-	public TaskRequest update() {
-		try {
-			PreparedStatement updateStatement = Connection.getConnection().prepareStatement(
-					"UPDATE `task_requests` SET `supervisor_id`=?, `worker_id`=?, `title`=?, `note`=? WHERE `id`=?");
-			
-			updateStatement.setString(1, this.supervisorID.toString());
-			updateStatement.setString(2, this.workerID.toString());
-			updateStatement.setString(3, this.title);
-			updateStatement.setString(4, this.note);
-			updateStatement.setString(5, this.id.toString());
-			
-			updateStatement.executeUpdate();
-		}
-		catch(SQLException e) {
-			e.printStackTrace();
-		}
+	public TaskRequest update() throws SQLException {
+		PreparedStatement updateStatement = Connection.getConnection().prepareStatement(
+				"UPDATE `task_requests` SET `supervisor_id`=?, `worker_id`=?, `title`=?, `note`=? WHERE `id`=?");
 		
+		updateStatement.setString(1, this.supervisorID.toString());
+		updateStatement.setString(2, this.workerID.toString());
+		updateStatement.setString(3, this.title);
+		updateStatement.setString(4, this.note);
+		updateStatement.setString(5, this.id.toString());
+		
+		updateStatement.executeUpdate();
+			
 		return this;
 	}
 	
-	public UUID delete() {
-		PreparedStatement deleteStatement;
-
-		try {
-			deleteStatement = Connection.getConnection().prepareStatement(
-					"DELETE FROM `task_requests` WHERE `id`=?");
+	public UUID delete() throws SQLException {
+		PreparedStatement deleteStatement = Connection.getConnection().prepareStatement(
+				"DELETE FROM `task_requests` WHERE `id`=?");
 		
-			deleteStatement.setString(1, this.id.toString());
-			
-			deleteStatement.executeUpdate();
-		}
-		catch(SQLException e) {
-			e.printStackTrace();
-		}
+		deleteStatement.setString(1, this.id.toString());
+		
+		deleteStatement.executeUpdate();
 		
 		return this.id;
 	}
@@ -98,61 +81,50 @@ public class TaskRequest {
 	
 	// STATIC FUNCTIONS
 	// Gets all task request that a user have based on user's id
-	public static List<TaskRequest> getAll(UUID userID) {
+	public static List<TaskRequest> getAll(UUID userID) throws SQLException {
 		ArrayList<TaskRequest> allTaskRequestList = new ArrayList<TaskRequest>();
 		
-		try {
-			PreparedStatement getAllStatement = Connection.getConnection().prepareStatement(
-					"SELECT * FROM `task_requests` WHERE `worker_id`=? OR `supervior_id`=?");
+		PreparedStatement getAllStatement = Connection.getConnection().prepareStatement(
+				"SELECT * FROM `task_requests` WHERE `worker_id`=? OR `supervior_id`=?");
+		
+		getAllStatement.setString(1, userID.toString());
+		getAllStatement.setString(2, userID.toString());
+		
+		ResultSet allTaskRequestTable = getAllStatement.executeQuery();
+		while(allTaskRequestTable.next()) {
+			UUID id				=	UUID.fromString(allTaskRequestTable.getString("id"));
+			UUID workerID		=	UUID.fromString(allTaskRequestTable.getString("worker_id"));
+			UUID supervisorID	=	UUID.fromString(allTaskRequestTable.getString("supervisor_id"));
+			String title		=	allTaskRequestTable.getString("title");
+			String note			=	allTaskRequestTable.getString("note");
 			
-			getAllStatement.setString(1, userID.toString());
-			getAllStatement.setString(2, userID.toString());
-			
-			ResultSet allTaskRequestTable = getAllStatement.executeQuery();
-			while(allTaskRequestTable.next()) {
-				UUID id				=	UUID.fromString(allTaskRequestTable.getString("id"));
-				UUID workerID		=	UUID.fromString(allTaskRequestTable.getString("worker_id"));
-				UUID supervisorID	=	UUID.fromString(allTaskRequestTable.getString("supervisor_id"));
-				String title		=	allTaskRequestTable.getString("title");
-				String note			=	allTaskRequestTable.getString("note");
-				
-				TaskRequest taskRequest = new TaskRequest(id, workerID, supervisorID, title, note);
-				allTaskRequestList.add(taskRequest);
-			}
-			
-		}
-		catch(SQLException e) {
-			e.printStackTrace();
+			TaskRequest taskRequest = new TaskRequest(id, workerID, supervisorID, title, note);
+			allTaskRequestList.add(taskRequest);
 		}
 		
 		return allTaskRequestList;
 	}
 	
 	// Gets a specific task request based on it's id
-	public static TaskRequest get(UUID id) {
+	public static TaskRequest get(UUID id) throws SQLException {
 		TaskRequest taskRequest = null;
 		
-		try {
-			PreparedStatement getStatement = Connection.getConnection().prepareStatement(
-					"SELECT * FROM `task_requests WHERE `id`=?");
-			
-			getStatement.setString(1, id.toString());
-			
-			ResultSet getTable = getStatement.executeQuery();
-			
-			if(getTable.next()) {
-				UUID workerID		=	UUID.fromString(getTable.getString("worker_id"));
-				UUID supervisorID	=	UUID.fromString(getTable.getString("supervisor_id"));
-				String title		=	getTable.getString("title");
-				String note			=	getTable.getString("note");
-				
-				taskRequest = new TaskRequest(id, workerID, supervisorID, title, note);
-			}
-		}
-		catch(SQLException e) {
-			e.printStackTrace();
-		}
+		PreparedStatement getStatement = Connection.getConnection().prepareStatement(
+				"SELECT * FROM `task_requests WHERE `id`=?");
 		
+		getStatement.setString(1, id.toString());
+		
+		ResultSet getTable = getStatement.executeQuery();
+		
+		if(getTable.next()) {
+			UUID workerID		=	UUID.fromString(getTable.getString("worker_id"));
+			UUID supervisorID	=	UUID.fromString(getTable.getString("supervisor_id"));
+			String title		=	getTable.getString("title");
+			String note			=	getTable.getString("note");
+			
+			taskRequest = new TaskRequest(id, workerID, supervisorID, title, note);
+		}
+	
 		return taskRequest;
 	}
 	
