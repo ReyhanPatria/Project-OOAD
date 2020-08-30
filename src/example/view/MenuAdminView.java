2652731baccf5baf947e055484b87d975d763339
ReyhanPatria <reyhan2patria@gmail.com>
2020-08-31 00:55:22 +0700

    Moved ActionListeners to ViewController

--- src/example/view/MenuAdminView.java
+++ src/example/view/MenuAdminView.java
@@ -1,52 +1,55 @@
 package example.view;
 
 import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import example.controller.FrameController;
-
 public class MenuAdminView extends JPanel implements ViewPanel {
 	private static final long serialVersionUID = 1L;
 	
-	private JLabel background;
+	private JLabel backgroundImage;
+	private JButton registerButton;
+	private JButton ViewButton;
+	private JButton logOutButton;
 	
 	public MenuAdminView() {
 		
 		this.setSize(new Dimension(1440,1024));
 		setLayout(null);
 		
-		JButton registerButton = new JButton("Register User");
-		registerButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				FrameController.changePanel(new RegisterUserPanel());
-			}
-		});
+		registerButton = new JButton("Register User");
 		registerButton.setBounds(508, 132, 106, 28);
-		add(registerButton);
+		this.add(registerButton);
 		
-		JButton ViewButton = new JButton("View All User");
+		ViewButton = new JButton("View All User");
 		ViewButton.setBounds(508, 202, 106, 28);
-		add(ViewButton);
+		this.add(ViewButton);
 		
-		JButton logOutButton = new JButton("Log Out");
+		logOutButton = new JButton("Log Out");
 		logOutButton.setBounds(508, 264, 106, 28);
-		logOutButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				FrameController.changePanel(new FirstPage());
-			}
-		});
-		add(logOutButton);
-					
+		this.add(logOutButton);				
 	}
 
+	// GETTERS
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public JLabel getBackgroundImage() {
+		return backgroundImage;
+	}
+
+	public JButton getRegisterButton() {
+		return registerButton;
+	}
+
+	public JButton getViewButton() {
+		return ViewButton;
+	}
+
+	public JButton getLogOutButton() {
+		return logOutButton;
+	}
 }
