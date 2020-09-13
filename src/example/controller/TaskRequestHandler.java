14b21f7704f038404f32fd5c2a52352b98cc9a8b
ReyhanPatria <reyhan2patria@gmail.com>
2020-09-13 20:11:21 +0700

    Added explanation comments

--- src/example/controller/TaskRequestHandler.java
+++ src/example/controller/TaskRequestHandler.java
@@ -39,22 +39,28 @@ public class TaskRequestHandler {
 	public TaskRequest createTaskRequest(String title, UUID supervisorID, UUID workerID, String note) 
 			throws IllegalArgumentException, SQLException {
 		// Validate parameters
+		// Validate title length, 5-20 characters
 		if(TaskHandler.validateTitleLength(title) == false) {
 			throw new IllegalArgumentException("Title's length must be 5-20 characters");
 		}
+		// Validate if the user that have workerID has worker role
 		else if(TaskHandler.validateWorkerID(workerID) == false) {
 			throw new IllegalArgumentException("Worker does not exist in database");
 		}
+		// Validate if the user that have supervisorID has supervisor role
 		else if(TaskHandler.validateSupervisorID(supervisorID) == false) {
 			throw new IllegalArgumentException("Supervisor does not exist in database");
 		}
+		// Validate note length, 0-50 characters
 		else if(TaskHandler.validateNoteLength(note) == false) {
 			throw new IllegalArgumentException("Note's length must be 0-50 characters");
 		}
 		
+		// Generate random uuid for task request
 		UUID id = UUID.randomUUID();
-		
+		// Create new task request object
 		TaskRequest newTaskRequest = new TaskRequest(id, workerID, supervisorID, title, note);
+		// Saves task request object into database
 		newTaskRequest.save();
 		
 		return newTaskRequest;
@@ -69,8 +75,9 @@ public class TaskRequestHandler {
 	
 	// Gets all task request that current user has
 	public List<TaskRequest> getAllTaskRequest() throws NoSuchObjectException, SQLException {
+		// Gets current user
 		User currentUser = Session.getInstance().getCurrentUser();
-		
+		// Gets task request list that have current user as supervisor or worker
 		List<TaskRequest> taskRequestList = TaskRequest.getAll(currentUser.getId());
 		
 		return taskRequestList;
@@ -79,19 +86,25 @@ public class TaskRequestHandler {
 	// Accepts a task request
 	// Deletes TaskRequest from database then creates a task from it, and gives a notification to the requester
 	public TaskRequest acceptTaskRequest(UUID taskRequestID) throws NoSuchObjectException, SQLException {
+		// Get task request data
 		TaskRequest taskRequest = TaskRequest.get(taskRequestID);
-		
 		String title = taskRequest.getTitle();
 		UUID supervisorID = taskRequest.getSupervisorID();
 		UUID workerID = taskRequest.getWorkerID();
 		String note = taskRequest.getNote();
 		
+		// Create task based on task request data and saves it into database
 		TaskHandler.getInstance().createTask(title, workerID, supervisorID, note);
 		
+		// Create notification for worker
+		// Gets supervisor name
 		String supervisorName = UserController.getInstance().getUser(supervisorID).getUsername();
+		// Create notification message: [Supervisor] has accepted your task request "[Task Title]"
 		String notificationMessage = String.format("%s has accepted your task request \"%s\"", supervisorName, title);
+		// Create notification object and saves it into database
 		NotificationController.getInstance().createNotification(workerID, notificationMessage);
 		
+		// Delete task request object from database
 		taskRequest.delete();
 		
 		return taskRequest;
@@ -100,16 +113,21 @@ public class TaskRequestHandler {
 	// Rejects a task request
 	// Deletes TaskRequest from database, and gives a notification to the requester
 	public TaskRequest rejectTaskRequest(UUID taskRequestID) throws SQLException {
+		// Get task request data
 		TaskRequest taskRequest = TaskRequest.get(taskRequestID);
-		
 		String title = taskRequest.getTitle();
 		UUID supervisorID = taskRequest.getSupervisorID();
 		UUID workerID = taskRequest.getWorkerID();
 		
+		// Delete task request object from database
 		taskRequest.delete();
 		
+		// Create notification for worker
+		// Gets supervisor name
 		String supervisorName = UserController.getInstance().getUser(supervisorID).getUsername();
+		// Create notification message: [Supervisor] has rejected your task request "[Task Title]"
 		String notificationMessage = String.format("%s has rejected your task request \"%s\"", supervisorName, title);
+		// Create notification object and saves it into database
 		NotificationController.getInstance().createNotification(workerID, notificationMessage);
 		
 		return taskRequest;
