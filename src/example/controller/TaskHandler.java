33ac1fb03375b3ec3c4311302d6c137076c5909b
ReyhanPatria <reyhan2patria@gmail.com>
2020-09-13 20:04:16 +0700

    Added explanation comments

--- src/example/controller/TaskHandler.java
+++ src/example/controller/TaskHandler.java
@@ -39,8 +39,11 @@ public class TaskHandler {
 	
 	// Get all task that current user have
 	public List<Task> getAllTask() throws NoSuchObjectException, SQLException {
+		// Gets current user
 		User currentUser = Session.getInstance().getCurrentUser();
+		// Gets all task that have current user as supervisor or worker
 		List<Task> taskList = Task.getAll(currentUser.getId());
+		
 		return taskList;
 	}
 	
@@ -55,41 +58,59 @@ public class TaskHandler {
 	public Task createTask(String title, UUID workerID, UUID supervisorID, String note) 
 			throws NoSuchObjectException, IllegalArgumentException, SQLException {
 		// Validate parameters
+		// Validate title length, 5-20 characters
 		if(validateTitleLength(title) == false) {
 			throw new IllegalArgumentException("Title's length must be 5-20 characters");
 		}
+		// Validate if the user that have workerID has worker role
 		else if(validateWorkerID(workerID) == false) {
 			throw new IllegalArgumentException("Worker does not exist in database");
 		}
+		// Validate if the user that have supervisorID has supervisor role
 		else if(validateSupervisorID(supervisorID) == false) {
 			throw new IllegalArgumentException("Supervisor does not exist in database");
 		}
+		// Validate note length, 0-50 characters
 		else if(validateNoteLength(note) == false) {
 			throw new IllegalArgumentException("Note's length must be 0-50 characters");
 		}
 		
+		// Gets current user
 		User currentUser = Session.getInstance().getCurrentUser();
 		Task newTask = null;
 		
+		// Checks current user role. If user is supervisor create task, if user is worker create task request
 		if(currentUser.getRole().equalsIgnoreCase("SUPERVISOR")) {
+			// Create new data for new task
 			UUID id = UUID.randomUUID();
 			Integer revisionCount = 0;
 			Integer score = 0;
 			Boolean isSubmitted = false;
 			Timestamp approvedAt = null;
 			
+			// Create new task object
 			newTask = new Task(id, workerID, supervisorID, title, revisionCount, score, isSubmitted, approvedAt, note);
+			// Save new task object into database
 			newTask.save();
 			
+			// Create notification to worker
+			// Getting supervisor's name to use in notification message
 			String supervisorName = UserController.getInstance().getUser(supervisorID).getUsername();
+			// Creating notification message: [Supervisor] has assigned you a new task "[Task Title]"
 			String notificationMessage = String.format("%s has assigned you a new task \"%s\"", supervisorName, title);
+			// Create notification with worker as receiver and saves it into database
 			NotificationController.getInstance().createNotification(workerID, notificationMessage);
 		}
 		else if(currentUser.getRole().equalsIgnoreCase("WORKER")) {
+			// Create task request
 			TaskRequestHandler.getInstance().createTaskRequest(title, supervisorID, workerID, note);
 			
+			// Create notification to supervisor
+			// Getting worker's name
 			String workerName = UserController.getInstance().getUser(workerID).getUsername();
+			// Create notification message: [Worker] has requested you to supervise a new task "[Task Title]"
 			String notificationMessage = String.format("%s has requested you to supervise a new task \"%s\"", workerName, title);
+			// Create notification with supervisor as receiver and saves it into database
 			NotificationController.getInstance().createNotification(supervisorID, notificationMessage);
 		}
 		
@@ -100,18 +121,23 @@ public class TaskHandler {
 	public Task updateTask(UUID taskID, String title, UUID workerID, UUID supervisorID, Integer score, String note) 
 			throws IllegalArgumentException, SQLException {
 		// Validate parameters
+		// Validate title length
 		if(validateTitleLength(title) == false) {
 			throw new IllegalArgumentException("Title's length must be 5-20 characters");
 		}
+		// Validate if the user that have workerID has worker role
 		else if(validateWorkerID(workerID) == false) {
 			throw new IllegalArgumentException("Worker does not exist in database");
 		}
+		// Validate if the user that have suprvisorID has supervisor role
 		else if(validateSupervisorID(supervisorID) == false) {
 			throw new IllegalArgumentException("Supervisor does not exist in database");
 		}
+		// Validate score, between 0-100
 		else if(validateScore(score) == false) {
 			throw new IllegalArgumentException("Score must be between 0-100");
 		}
+		// Validate note length
 		else if(validateNoteLength(note) == false) {
 			throw new IllegalArgumentException("Note's length must be 0-50 characters");
 		}
@@ -126,13 +152,16 @@ public class TaskHandler {
 		task.setScore(score);
 		task.setNote(note);
 		
-		// Update task in database
+		// Update task and saves it into database
 		task.update();
 		
-		// Sends notification for supervisor and worker
+		// Sends notification for supervisor and worker: [Task Owner] has updated information on task "[Task Title]"
+		// Getting supervisor's name as task owner
 		String supervisorName = UserController.getInstance().getUser(supervisorID).getUsername();
+		// Create notification to supervisor and saves it into database
 		NotificationController.getInstance().createNotification(
 				supervisorID, String.format("%s has updated information on task \"%s\"", supervisorName, title));
+		// Create notification to worker and saves it into database
 		NotificationController.getInstance().createNotification(
 				workerID, String.format("%s has updated information on task \"%s\"", supervisorName, title));
 		
@@ -152,41 +181,51 @@ public class TaskHandler {
 		// Gets worker and supervisor id
 		UUID supervisorID = task.getSupervisorID();
 		UUID workerID = task.getWorkerID();
-		// Gets supervisor name
+		// Gets supervisor name as task owner
 		String supervisorName = UserController.getInstance().getUser(supervisorID).getUsername();
-		// Sends notification for supervisor and worker
-		NotificationController.getInstance().createNotification(
-				supervisorID, String.format("%s has deleted on task \"%s\"", supervisorName, title));
-		NotificationController.getInstance().createNotification(
-				workerID, String.format("%s has deleted task \"%s\"", supervisorName, title));
+		// Create notification message: [Task Owner] has deleted task "[Task Title]"
+		String notificationMessage  = String.format("%s has deleted on task \"%s\"", supervisorName, title);
+		// Sends notification for supervisor and saves it into database
+		NotificationController.getInstance().createNotification(supervisorID, notificationMessage);
+		// Create notification for worker and saves it into database
+		NotificationController.getInstance().createNotification(workerID, notificationMessage);
 	}
 	
 	// Approve of a Task
 	// Sets Task's score and approved timestamp then updates the database, gives a notification
 	public Task approveTask(UUID taskID, Integer score) throws IllegalArgumentException, SQLException {
 		// Validate parameters
+		// Validate score, between 0-100
 		if(validateScore(score) == false) {
 			throw new IllegalArgumentException("Score must be between 0-100");
 		}
 		
+		// Getting task's data
 		Task task = getTask(taskID);
 		String title = task.getTitle();
 		UUID supervisorID = task.getSupervisorID();
 		UUID workerID = task.getSupervisorID();
 		Boolean isSubmitted = task.getIsSubmitted();
 		
-		// Check if task have been submitted
+		// Check if task have been submitted, if not throw error
 		if(isSubmitted == false) {
 			throw new IllegalArgumentException("Task hasn't been submitted");
 		}
 		
+		// Updates task's score to new score
 		task.setScore(score);
+		// Update task's approve timestamp to now
 		task.setApprovedAt(Timestamp.from(Instant.now()));
 		
+		// Updates task and saves it into database
 		task.update();
 		
+		// Create notification to worker
+		// Getting supervisor name
 		String supervisorName = UserController.getInstance().getUser(supervisorID).getUsername();
+		// Creates notification message: [Supervisor] has approved your task "[Task Title]" 
 		String notificationMessage = String.format("%s has approved your task \"%s\"", supervisorName, title);
+		// Create notification for worker and saves it into database
 		NotificationController.getInstance().createNotification(workerID, notificationMessage);
 		
 		return task;
@@ -195,26 +234,38 @@ public class TaskHandler {
 	// Request a Task revision
 	// Adds 1 to Task's revision count and set submitted status to false, gives a notification
 	public Task requestTaskRevision(UUID taskID) throws SQLException {
+		// Gets task data
 		Task task = getTask(taskID);
 		String title = task.getTitle();
 		UUID supervisorID = task.getSupervisorID();
 		UUID workerID = task.getSupervisorID();
 		Integer revisionCount = task.getRevisionCount();
 		Boolean isSubmitted = task.getIsSubmitted();
+		Timestamp approvedDate = task.getApprovedAt();
 		
-		// Check if task have been submitted
+		// Check if task have been submitted, if not throw error
 		if(isSubmitted == false) {
 			throw new IllegalArgumentException("Task hasn't been submitted");
 		}
+		// Check if task has been approved, if yes throw error
+		else if(approvedDate != null) {
+			throw new IllegalArgumentException("Task has already been approved");
+		}
 		
+		// Add 1 to revision count
 		task.setRevisionCount(revisionCount + 1);
+		// Set submitted to false
 		task.setIsSubmitted(false);
-		task.setApprovedAt(null);
 		
+		// Update task and saves it to database
 		task.update();
 		
+		// Create notification to worker
+		// Gets supervisor name as task owner
 		String supervisorName = UserController.getInstance().getUser(supervisorID).getUsername();
+		// Create notification message: [Task Owner] has updated information on task "[Task Title]"
 		String notificationMessage = String.format("%s has requested you a revision on task \"%s\"", supervisorName, title);
+		// Create notification and saves it into database
 		NotificationController.getInstance().createNotification(workerID, notificationMessage);
 		
 		return task;
@@ -223,17 +274,24 @@ public class TaskHandler {
 	// Submit a Task
 	// Sets Task's submitted status to true then updates the database, and gives a notification
 	public Task submitTask(UUID taskID) throws SQLException {
+		// Gets task data
 		Task task = getTask(taskID);
 		String title = task.getTitle();
 		UUID supervisorID = task.getSupervisorID();
 		UUID workerID = task.getSupervisorID();
 		
+		// Set isSumbitted to true
 		task.setIsSubmitted(true);
 		
+		// Updates task and saves it into database
 		task.update();
 		
+		// Creates notification to supervisor
+		// Gets worker name
 		String workerName = UserController.getInstance().getUser(workerID).getUsername();
+		// Create notification message: [Worker] has submitted "[Task Title]"
 		String notificationMessage = String.format("%s has submitted \"%s\"", workerName, title);
+		// Create notification and saves it into database
 		NotificationController.getInstance().createNotification(supervisorID, notificationMessage);
 		
 		return task;
@@ -242,6 +300,7 @@ public class TaskHandler {
 	// Search for a task based on title name or supervisor/worker username
 	// Returns a list of task that best fits the query
 	public List<Task> searchTask(String query) throws NoSuchObjectException, SQLException {
+		// Gets task list based on search query
 		List<Task> taskList = Task.search(query);
 		
 		return taskList;
@@ -249,6 +308,7 @@ public class TaskHandler {
 	
 	// Sort the task list based on a column and direction
 	public List<Task> sortTask(Task.SortBy sortBy, Task.SortDirection direction) throws NoSuchObjectException, SQLException {
+		// Sort task based on sort by column and sort direction
 		List<Task> taskList = Task.sort(sortBy, direction);
 		
 		return taskList;
