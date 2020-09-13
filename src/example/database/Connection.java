1503a5eb89eea3a5d6f6eb54c87f0f45895a0082
ReyhanPatria <reyhan2patria@gmail.com>
2020-09-13 20:52:35 +0700

    Added explanation comments

--- src/example/database/Connection.java
+++ src/example/database/Connection.java
@@ -3,8 +3,30 @@ package example.database;
 import java.sql.DriverManager;
 
 public class Connection {
+	// STATIC ATTRIBUTES
+	// Instance of connection
 	private static java.sql.Connection con;
 	
+	
+	
+	
+	
+	// STATIC FUNCTIONS
+	// Get instance of connection
+	public static java.sql.Connection getConnection() {
+		if(con == null) {
+			new Connection();
+		}
+		
+		return con;
+	}
+	
+	
+	
+	
+	
+	// NON-STATIC FUNCTIONS
+	// Constructor
 	public Connection() {
 		try {
 			con = DriverManager.getConnection(String.format("jdbc:%s://localhost/%s", Environment.db_product, Environment.db_name), 
@@ -13,13 +35,5 @@ public class Connection {
 		catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
-	
-	public static java.sql.Connection getConnection() {
-		if(con == null) {
-			new Connection();
-		}
-		
-		return con;
 	}
 }
